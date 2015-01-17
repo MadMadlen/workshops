257365afa64a83f8426e20ba83911c02b2ab6fa0
Magdalena Poprawa <magdalenapoprawa@gmail.com>
2015-01-17 19:51:39 +0100

    authenticate in products_controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,9 @@ class ProductsController < ApplicationController
   expose(:review) { Review.new }
   expose_decorated(:reviews, ancestor: :product)
 
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :check_if_owner, only: [:edit, :update]
+
   def index
   end
 
@@ -46,5 +49,12 @@ class ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:title, :description, :price, :category_id)
+  end
+
+  def check_if_owner
+    if product.user != current_user
+      redirect_to category_product_url(category, product)
+      flash[:error] = 'You are not allowed to edit this product.'
+    end
   end
 end
