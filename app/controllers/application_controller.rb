4f5288003db4178ba1a6bff1e58111ec7cd4504c
Magdalena Poprawa <magdalenapoprawa@gmail.com>
2015-01-17 19:22:15 +0100

    user model, devise views, controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
+
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
   end
 
-  protect_from_forgery with: :exception
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:lastname, :firstname, :email, :password, :password_confirmation) }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
     end
   end
 
+  devise_for :users, path: "auth", path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon' }
+
   root 'categories#index'
 end
