e648dc7b8bdbbad079ce09dfd5687815c1962c8b
Magdalena Poprawa <magdalenapoprawa@gmail.com>
2015-01-18 21:26:19 +0100

    fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
 
-  expose(:review)
+  expose(:reviews)
   expose(:user)
 
+  before_action :set_reviews
+
   def show
+  end
+
+  private
+
+  def set_reviews
+    @reviews = user.reviews.limit(5)
   end
 
 end
